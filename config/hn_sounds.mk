375690b47100b4c9176dab55ac91dbfcd46fbcb2
houstonn <houstonn@gmail.com>
2013-05-19 19:07:21 -0400

    add my sound package

---
+++ config/hn_sounds.mk
@@ -0,0 +1,11 @@
+# houstonn extra sounds
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/media/sounds/xperia.ogg:system/media/audio/ringtones/xperia.ogg \
+    vendor/pa/prebuilt/common/media/sounds/xperia_z_breeze.ogg:system/media/audio/ringtones/xperia_z_breeze.ogg \
+    vendor/pa/prebuilt/common/media/sounds/xperia_z_rise.ogg:system/media/audio/ringtones/xperia_z_rise.ogg \
+    vendor/pa/prebuilt/common/media/sounds/xperia1_z.ogg:system/media/audio/ringtones/xperia1_z.ogg \
+    vendor/pa/prebuilt/common/media/sounds/xperia2_z.ogg:system/media/audio/ringtones/xperia2_z.ogg \
+    vendor/pa/prebuilt/common/media/sounds/Whistle.ogg:system/media/audio/notifications/s_whistle.ogg
+
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/apk/MiuiMusic232.apk:system/app/MiuiMusic232.apk
